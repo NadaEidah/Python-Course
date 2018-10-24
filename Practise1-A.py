3b80419b28e2f623b27ff4c6fcda327a2cac3266
NadaEidah <44302867+NadaEidah@users.noreply.github.com>
2018-10-24 23:17:31 +0300

    Add files via upload

---
+++ Practise1-A.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "user_input = input (\"Enter your name:\")\n",
+    "user_input1 = input (\"Enter the first Number\")\n",
+    "user_input2 =input (\"Enter the second number\")\n",
+    "print (f\"سلام عليكم, {user_input}\")\n",
+    "print (int(user_input1)+int(user_input2))\n",
+    "print (int(user_input1)-int(user_input2))\n",
+    "print (int(user_input1)*int(user_input2))\n",
+    "print (int(user_input1)/int(user_input2))\n",
+    "print (int(user_input1)%int(user_input2))\n",
+    "print (int(user_input1)**int(user_input2))"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
