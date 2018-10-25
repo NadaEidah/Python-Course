3c7946ce827167cb990c44f3b19c459bec0dc90a
NadaEidah <44302867+NadaEidah@users.noreply.github.com>
2018-10-25 19:49:18 +0300

    Add files via upload

---
+++ Week 1/Practise1-C.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {
+    "scrolled": true
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Input the temperatares fahrenheit to convert in celsius or vise versa\n",
+      "Fahrenheit Temperatares:5\n",
+      "-15.0T(ْC)\n",
+      "Celsius Temperatares:-15\n",
+      "5.0T(ْF)\n"
+     ]
+    }
+   ],
+   "source": [
+    "print (\"Input the temperatares fahrenheit to convert in celsius or vise versa\")\n",
+    "F= float(input(\"Fahrenheit Temperatares:\"))\n",
+    "print ((str((F-32)/1.8)) + \"T(ْC)\")\n",
+    "C= float(input(\"Celsius Temperatares:\"))\n",
+    "print((str((1.8*C)+32)) + \"T(ْF)\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
